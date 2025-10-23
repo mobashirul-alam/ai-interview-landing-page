25dcb5e2943796c9384e1ab6fe45caa1e229ea16
Md Mobashirul Alam Seam <96857205+mobashirul-alam@users.noreply.github.com>
2025-10-23 12:32:25 +0600

    chore: update dependencies in package.json
    - Added @vercel/speed-insights version 1.2.0
    - Added framer-motion version 12.23.24
    - Removed duplicate framer-motion entry

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@/components/Header";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import type React from "react";
@@ -41,6 +42,7 @@ export default function RootLayout({
                     {children}
                 </ThemeProvider>
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
